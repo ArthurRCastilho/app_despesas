75907b7c2bab677e25a328bbd9b84d6c9b795a09
ArthurLVidal <arthurodri2017@gmail.com>
2024-05-06 17:56:29 -0300

    Adicionando Lista Personalizada: Adicionando uma Row para as listas, e dividindo essa row em uma Column e um Container. Foi adicionado também estilo ao Container, que está responsável de mostrar o valor da despesa.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:app_despesas/models/transaction.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -48,7 +46,30 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: _transactions.map((tr) {
               return Card(
-                child: Text(tr.title),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(tr.value.toString()),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tr.title),
+                        Text(tr.date.toString())
+                      ],
+                    )
+                  ],
+                ),
               );
             }).toList(),
           ),
